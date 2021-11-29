4e6e8b437f871e0cd54b2bb1a0d00f9e9eb9a887
Gary <geco.yak77@gmail.com>
2021-11-29 14:25:59 -0400

    proyecto_bd v1.6.15

--- añadir_nota.php
+++ añadir_nota.php
@@ -21,9 +21,10 @@ $cod_materia = $_REQUEST['cod_materia'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>admin</title>
+    <title>Actualizar notas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="assets/GCodDev_Circular.png" type="image/x-icon">
 </head>
 
 <body>
--- profesores.php
+++ profesores.php
@@ -19,9 +19,10 @@ foreach ($res as $dato):
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>admin</title>
+    <title>Datos profesor</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="assets/GCodDev_Circular.png" type="image/x-icon">
 </head>
 
 <body>
